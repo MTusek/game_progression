2b075bf814e383beff131d7740efba848c9dc03a
MTusek <marko.tusek02@gmail.com>
2025-02-01 17:50:53 +0100

    Priprema za prelazak podataka sa servera na aplikaciju

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,3 +1,5 @@
+val implementation: Unit = Unit
+
 plugins {
     id("com.android.application")
 }
@@ -32,11 +34,13 @@ android {
 }
 
 dependencies {
-
     implementation("androidx.appcompat:appcompat:1.7.0")
     implementation("com.google.android.material:material:1.12.0")
     implementation("androidx.constraintlayout:constraintlayout:2.2.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.2.1")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
+    implementation ("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation ("com.squareup.retrofit2:converter-gson:2.5.0")
+
 }
---
+++ app/src/main/java/com/example/gameprogression/retrofit/RetrofitService.java
@@ -0,0 +1,8 @@
+package com.example.gameprogression.retrofit;
+
+import retrofit2.Retrofit;
+
+public class RetrofitService {
+
+    private Retrofit retrofit;
+}
